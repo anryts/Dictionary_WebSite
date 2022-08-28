9f5bea7f9ae03a149a49a1c79bea9d12c5881042
Andrii Hrytsenko <gritsenko99beta@gmail.com>
2022-08-28 20:29:38 +0300

    Add simple navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import Input_Label from "./components/Input_Label";
-
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Main_page from "./components/pages/Main_page";
+import Dictionary_page from "./components/pages/Dictionary_page";
 function App() {
   return (
-    <div className="App">
-        <Input_Label/>
-    </div>
+    <BrowserRouter>
+      <Route path="/" >
+          <Main_page />
+        </Route>
+        <Route path="/dictionary" >
+            <Dictionary_page />
+            </Route>
+    </BrowserRouter>
   );
 }
 
